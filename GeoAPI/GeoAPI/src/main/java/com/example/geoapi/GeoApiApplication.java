7dfeae842640d3216c8c08e826a95f4f51d4e0e0
Jakub Gola <kuba300901@gmail.com>
2022-05-30 09:14:12 +0200

    swagger homepage fixed

--- GeoAPI/GeoAPI/src/main/java/com/example/geoapi/GeoApiApplication.java
+++ GeoAPI/GeoAPI/src/main/java/com/example/geoapi/GeoApiApplication.java
@@ -1,23 +1,23 @@
 package com.example.geoapi;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
-import javax.print.Doc;
+import java.util.Collections;
 
 @SpringBootApplication
 @EnableSwagger2
 @EnableWebMvc
+@Configuration
 public class GeoApiApplication {
 
     public static void main(String[] args) {
@@ -29,7 +29,18 @@ public class GeoApiApplication {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .paths(PathSelectors.ant("/location/**"))
-                .build();
+                .build()
+                .apiInfo(apiInfo());
+    }
+    private ApiInfo apiInfo() {
+        return new ApiInfo("Coordinates API",
+                "Location API for EnigmaSOI",
+                "1.0",
+                "Terms of service",
+                new Contact("Jakub", "https://github.com/GolaJakub/EnigmaSOI", "GolaJakub@protonmail.com"),
+                "License of API",
+                "API license URL",
+                Collections.emptyList());
     }
 
 
